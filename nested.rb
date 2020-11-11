6606c3fa34b0c97537197dee894f5051cc5d4299
Shreya Sridhar <shreyamohak@outlook.com>
2020-11-11 20:26:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -15,7 +14,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +35,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +54,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -81,8 +80,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
-end
+  programmer_hash[:yukihiro_matsumoto] = {
+#   :known_for => "Ruby",
+#   :languages => ["LISP", "C"]
+#  }
+return programmer_hash
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
@@ -103,7 +105,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
@@ -125,5 +127,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  return programmer_hash
 end
